d27c60de0472b42d7d950d432b55edff02543043
Pasindu Nimsara Vidanapathirana <rvppasindu@gmail.com>
2026-09-24 20:56:36 +0530

    Update allUsers query and add userId log

--- Library_Management_System/backend/controller/allUsers.js
+++ Library_Management_System/backend/controller/allUsers.js
@@ -1,25 +1,25 @@
-const userModel = require('../models/userModel');
+const userModel = require("../models/userModel")
 
-async function allUsers(req, res) {
-  try {
-    const allUsers = await userModel
-      .find()
-      .select('-password')
-      .sort({ createdAt: -1 });
+async function allUsers(req,res) {
+    try{ 
+        console.log("userid all Users",req.userId)
 
-    res.json({
-      message: 'All User',
-      data: allUsers,
-      success: true,
-      error: false
-    });
-  } catch (err) {
-    res.status(400).json({
-      message: err.message || err,
-      error: true,
-      success: false
-    });
-  }
+        const allUsers = await userModel.find().sort({ createdAt: -1 })
+
+        res.json({
+            message : "All User",
+            data : allUsers,
+            success : true,
+            error : false
+        })
+
+    }catch(err){
+        res.status(400).json({
+            message : err.message || err,
+            error : true,
+            success : false,
+        })
+    }
 }
 
-module.exports = allUsers;
+module.exports = allUsers
